b82b043ddb0d5f0fc2096e9cd4729f4b17654d6c
Matt Martinez <matt@Matts-MacBook-Pro.local>
2022-07-31 08:04:02 -0700

    fixed install file

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -7,7 +7,7 @@ window.addEventListener('beforeinstallprompt', (event) => {});
   butInstall.classList.toggle('hidden', false);
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
   const promptEvent = window.deferredPrompt;
 
   if (!promptEvent) {
@@ -17,6 +17,8 @@ butInstall.addEventListener('click', async () => {});
 
   window.deferredPrompt = null;
   butInstall.classList.toggle('hidden', true);
+});
+    
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
